e31941984be10b3ab3ce628d0f0fafa65705696d
thnakon <thnakon.d@gmail.com>
2026-03-30 16:12:51 +0700

    fix: make citation-generator route public

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
+Route::view('citation-generator', 'citation-generator')->name('citation-generator');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
-    Route::view('citation-generator', 'citation-generator')->name('citation-generator');
 });
 
 require __DIR__.'/settings.php';
