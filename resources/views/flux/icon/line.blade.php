9c3f73dd86733f2169716d78d1d01a9a66bfacf8
thnakon <thnakon.d@gmail.com>
2026-03-31 09:21:36 +0700

    feat: add social icons to share dropdown

---
+++ resources/views/flux/icon/line.blade.php
@@ -0,0 +1,7 @@
+<svg {{ $attributes->merge(['class' => 'size-5', 'fill' => 'none', 'stroke' => 'currentColor', 'stroke-width' => '2', 'stroke-linecap' => 'round', 'stroke-linejoin' => 'round', 'viewBox' => '0 0 24 24']) }}>
+    <path d="M21 11.5c0-4.142-3.806-7.5-8.5-7.5S4 7.358 4 11.5c0 3.708 3.036 6.817 7.141 7.411l-.541 2.29s-.1 1 1 0-.5-1 4-1c3.806 0 8.5-3.358 8.5-7.5z"></path>
+    <path d="M8.5 9h-1v5h1"></path>
+    <path d="M10 9v5h2"></path>
+    <path d="M14 9h-1v5h1"></path>
+    <path d="M18.5 9h-2.5v5h2.5"></path>
+</svg>
---
+++ resources/views/flux/icon/twitter.blade.php
@@ -0,0 +1,4 @@
+<svg {{ $attributes->merge(['class' => 'size-5', 'fill' => 'none', 'stroke' => 'currentColor', 'stroke-width' => '2', 'stroke-linecap' => 'round', 'stroke-linejoin' => 'round', 'viewBox' => '0 0 24 24']) }}>
+    <path d="M4 4l11.733 16h4.267l-11.733 -16z"></path>
+    <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772"></path>
+</svg>
